b64d89b49ab94c7399e4134be9bd4b9704a94b26
masa3 <masa@hashicorp.com>
2020-09-01 12:53:41 +0900

    test s3

--- lambda.tf
+++ lambda.tf
@@ -6,6 +6,9 @@ terraform {
   }
 }
 
+variable artifact {
+  default = "masa/terraform_serverless/example.zip"
+}
 
 provider "aws" {
   region = "ap-northeast-1"
@@ -16,7 +19,8 @@ resource "aws_lambda_function" "example" {
 
   # The bucket name as created earlier with "aws s3api create-bucket"
   s3_bucket = "hashicorp-japan"
-  s3_key    = "masa/terraform_serverless/example.zip"
+  # s3_key    = "masa/terraform_serverless/example.zip"
+  s3_key = var.artifact
 
   # "main" is the filename within the zip file (main.js) and "handler"
   # is the name of the property under which the handler function was
@@ -29,14 +33,14 @@ resource "aws_lambda_function" "example" {
 }
 
 resource "aws_lambda_permission" "apigw" {
-   statement_id  = "AllowAPIGatewayInvoke"
-   action        = "lambda:InvokeFunction"
-   function_name = aws_lambda_function.example.function_name
-   principal     = "apigateway.amazonaws.com"
+  statement_id  = "AllowAPIGatewayInvoke"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.example.function_name
+  principal     = "apigateway.amazonaws.com"
 
-   # The "/*/*" portion grants access from any method on any resource
-   # within the API Gateway REST API.
-   source_arn = "${aws_api_gateway_rest_api.example.execution_arn}/*/*"
+  # The "/*/*" portion grants access from any method on any resource
+  # within the API Gateway REST API.
+  source_arn = "${aws_api_gateway_rest_api.example.execution_arn}/*/*"
 }
 
 # IAM role which dictates what other AWS services the Lambda function
@@ -93,30 +97,30 @@ resource "aws_api_gateway_integration" "lambda" {
 }
 
 resource "aws_api_gateway_method" "proxy_root" {
-   rest_api_id   = aws_api_gateway_rest_api.example.id
-   resource_id   = aws_api_gateway_rest_api.example.root_resource_id
-   http_method   = "ANY"
-   authorization = "NONE"
+  rest_api_id   = aws_api_gateway_rest_api.example.id
+  resource_id   = aws_api_gateway_rest_api.example.root_resource_id
+  http_method   = "ANY"
+  authorization = "NONE"
 }
 
 resource "aws_api_gateway_integration" "lambda_root" {
-   rest_api_id = aws_api_gateway_rest_api.example.id
-   resource_id = aws_api_gateway_method.proxy_root.resource_id
-   http_method = aws_api_gateway_method.proxy_root.http_method
+  rest_api_id = aws_api_gateway_rest_api.example.id
+  resource_id = aws_api_gateway_method.proxy_root.resource_id
+  http_method = aws_api_gateway_method.proxy_root.http_method
 
-   integration_http_method = "POST"
-   type                    = "AWS_PROXY"
-   uri                     = aws_lambda_function.example.invoke_arn
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+  uri                     = aws_lambda_function.example.invoke_arn
 }
 
 resource "aws_api_gateway_deployment" "example" {
-   depends_on = [
-     aws_api_gateway_integration.lambda,
-     aws_api_gateway_integration.lambda_root,
-   ]
+  depends_on = [
+    aws_api_gateway_integration.lambda,
+    aws_api_gateway_integration.lambda_root,
+  ]
 
-   rest_api_id = aws_api_gateway_rest_api.example.id
-   stage_name  = "test"
+  rest_api_id = aws_api_gateway_rest_api.example.id
+  stage_name  = "test"
 }
 
 # Output
