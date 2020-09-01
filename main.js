ee1e881d0a57aa672620745d8befd115ae42690b
masa3 <masa@hashicorp.com>
2020-09-01 14:34:13 +0900

    code update

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ exports.handler = function (event, context, callback) {
     headers: {
       'Content-Type': 'text/html; charset=utf-8',
     },
-    body: '<p>ようこそTerraformの世界へ！v3 </p>',
+    body: '<p>ようこそTerraformの世界へ！</p>',
   }
   callback(null, response)
 }
