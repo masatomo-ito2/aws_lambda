b325a24f931e9b2f86fdd0627fdcf91bedd8a190
masa3 <masa@hashicorp.com>
2020-09-02 10:46:41 +0900

    pre run

--- main.js
+++ main.js
@@ -1,12 +1,13 @@
 'use strict'
 
+
 exports.handler = function (event, context, callback) {
   var response = {
     statusCode: 200,
     headers: {
       'Content-Type': 'text/html; charset=utf-8',
     },
-    body: '<p>ようこそTerraformの世界へ！test</p>',
+    body: '<p>ようこそTerraformの世界へ！</p>',
   }
   callback(null, response)
 }
